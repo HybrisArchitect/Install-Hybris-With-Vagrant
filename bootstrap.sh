5623092d507f6f9b67829c7d818c83492b65ae43
Marc Raygoza <info@hybrisarchitect.com>
2017-06-22 18:22:31 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -17,7 +17,6 @@ wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2F
 tar xzf jdk-8u131-linux-x64.tar.gz
 cd /opt/jdk1.8.0_131/
 alternatives --install /usr/bin/java java /opt/jdk1.8.0_131/bin/java 2
-
 alternatives --install /usr/bin/jar jar /opt/jdk1.8.0_131/bin/jar 2
 alternatives --install /usr/bin/javac javac /opt/jdk1.8.0_131/bin/javac 2
 alternatives --set jar /opt/jdk1.8.0_131/bin/jar
